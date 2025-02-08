9e1a9221a8de5810534fdfbc09ddfd43325df403
wr1159 <chuweiron18@gmail.com>
2025-02-08 22:56:47 +0000

    Play: Add check to prevent users from playing twice

--- frontend/src/app/play/page.tsx
+++ frontend/src/app/play/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import GameBoard from "@/components/gameboard";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import {
     Carousel,
@@ -10,14 +11,16 @@ import {
 } from "@/components/ui/carousel";
 import { Spinner } from "@/components/ui/spinner";
 import { guessTheHumanAbi, guessTheHumanAddress } from "@/generated";
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { useChainId, useReadContract } from "wagmi";
+import { useAccount, useChainId, useReadContract } from "wagmi";
 
 export default function Play() {
     const searchParams = useSearchParams();
     const id = parseInt(searchParams.get("id") || "1");
     const chainId = useChainId();
+    const account = useAccount();
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [gameData, setGameData] = useState<
         readonly [bigint, bigint, `0x${string}`, boolean] | undefined
@@ -41,6 +44,13 @@ export default function Play() {
         functionName: "getFlatBoard",
         args: [BigInt(id)],
     });
+    const { data: playerMovesData } = useReadContract({
+        address:
+            guessTheHumanAddress[chainId as keyof typeof guessTheHumanAddress],
+        abi: guessTheHumanAbi,
+        functionName: "getPlayerMoves",
+        args: [BigInt(id), account.address as `0x${string}`],
+    });
 
     // Once both fetches complete, update state
     useEffect(() => {
@@ -55,6 +65,24 @@ export default function Play() {
         return (
             <div className="max-w-5xl w-screen h-screen flex items-center justify-center mx-auto">
                 <Spinner size="xl" />
+            </div>
+        );
+
+    if (playerMovesData?.length !== 0)
+        return (
+            <div className="max-w-5xl w-screen h-screen flex items-center justify-center mx-auto flex-col">
+                <h1 className="text-3xl font-semibold text-center">
+                    You have already attempted today. Please come again
+                    tomorrow.
+                </h1>
+                <Link
+                    href="/guess"
+                    className="flex items-center justify-center"
+                >
+                    <Button className="mt-4" size={"lg"}>
+                        Try Guess Some Humans.
+                    </Button>
+                </Link>
             </div>
         );
 
